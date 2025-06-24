5322e790da4bbffb221bcf96616e0041363777f2
Muhita <muhitasenthil13@gamil.com>
2025-06-24 13:59:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var numberInshape =1;
+var oldValue = 0;
+let button = document.getElementById("button");
+
+button.onclick = () => {
+    let n= document.getElementById("number").value;
+    let circle=document.getElementById("circle");
+    let square=document.getElementById("square");
+    let rectangle=document.getElementById("rectangle");
+
+    let box = document.getElementById("box");
+
+    let j;
+    n=Number(oldValue)+Number(n);
+    for(j=numberInshape; j<=n;j++)
+    {
+        var shape = document.createElement("div");
+        shape.innerHTML += numberInshape;
+        if(square.checked)
+            shape.classList.add("square");
+        else if(circle.checked)
+            shape.classList.add("circle");
+        else if(rectangle.checked)
+            shape.classList.add("rectangle");
+        else
+        document.write("Invalid Input");
+    box.appendChild(shape);
+    numberInshape++;
+    oldValue = document.getElementById("box").lastElementChild.innerHTML;
+    }
+
+}
